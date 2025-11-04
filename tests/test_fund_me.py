5978f55ac3d5dceb9fa3c437aa7ec5bfe162c8b1
OlirahetaX <oliveriraheta57@gmail.com>
2025-11-03 22:20:34 -0600

    feat: new test test_only_owner_can_withdraw

--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -1,5 +1,7 @@
-from scripts.helpful_scripts import get_Account
+from scripts.helpful_scripts import get_Account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from scripts.deploy import deploy_fund_me
+from ape import networks, accounts
+import pytest
 
 def test_can_fund_and_withdraw():
     account = get_Account()
@@ -9,3 +11,12 @@ def test_can_fund_and_withdraw():
     assert fund_me.addressToAmountFunded(account.address) == entrance_fee
     tx2 = fund_me.withdraw(sender=account)
     assert fund_me.addressToAmountFunded(account.address) == 0
+    
+def test_only_owner_can_withdraw():
+    if networks.active_provider.network.name.lower() not in [env.lower() for env in LOCAL_BLOCKCHAIN_ENVIRONMENTS]:
+        pytest.skip("Only for local testing")
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.test_accounts[1]
+    with pytest.raises(Exception) as exc_info:
+        fund_me.withdraw(sender=bad_actor)
+    
